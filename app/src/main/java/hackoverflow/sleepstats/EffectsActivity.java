bf75389d565f2f60ed18f7d8440bd325f6efea50
Grace Hong <ghong97@stanford.edu>
2017-04-15 13:36:52 -0700

    Added methods to update Effects View based on data pulled from intent

--- app/src/main/java/hackoverflow/sleepstats/EffectsActivity.java
+++ app/src/main/java/hackoverflow/sleepstats/EffectsActivity.java
@@ -1,5 +1,7 @@
 package hackoverflow.sleepstats;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +9,8 @@ import android.os.Bundle;
 public class EffectsActivity extends AppCompatActivity {
 
     SQLiteDatabase db;
-
+    private static final int MAX_SLEEP = 13;
+    private static final int MIN_SLEEP = 2;
     /**
      * FUNCTION: onCreate
      * Overrides the onCreate method to initialize the database
@@ -17,6 +20,64 @@ public class EffectsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_effects);
-        openOrCreateDatabase("")
+        openOrCreateDatabase("sleepDB", MODE_PRIVATE, null);
+
+        //initializes database
+        db = openOrCreateDatabase("My Cities", MODE_PRIVATE, null);
+        Cursor tablesCursor = db.rawQuery(
+                "SELECT * FROM sqlite_master WHERE type='table' AND name='cities';",
+                null);
+        if (tablesCursor.getCount() == 0){
+            setUpDatabase();
+        }
+
+        //retrieves and interprets hours slept
+        Intent intent = getIntent();
+        double totalTimeVal = intent.getDoubleExtra("totalTimeVal", 0.0);
+
+        //updates activity based on totalTimeVal
+        updateActivity(totalTimeVal);
+    }
+
+    /**
+     * FUNCTION: updateActivity
+     * Private helper function that updates the activity by parsing bracket given by passed in
+     * value and adding relevant information from database
+     */
+    private void updateActivity (double totalTimeVal){
+        //rounds totalTimeVal to nearest bracket
+        int roundedTimeVal = (int) Math.round(totalTimeVal);
+        if (roundedTimeVal > MAX_SLEEP) roundedTimeVal = MAX_SLEEP;
+        if (roundedTimeVal < MIN_SLEEP) roundedTimeVal = MIN_SLEEP;
+
+        //retrieves information from database
+        String overviewStr;
+        String symptomStr;
+        String adviceStr;
+
+        String cmd = "SELECT overview, symptom, advice FROM effects WHERE bracket = ?;";
+
+        Cursor cur = db.rawQuery(cmd, new String[]{Integer.toString(roundedTimeVal)});
+        overviewStr = cur.getString(cur.getColumnIndex("overview"));
+        symptomStr = cur.getString(cur.getColumnIndex("symptom"));
+        adviceStr = cur.getString(cur.getColumnIndex("advice"));
+
+        //adds information to view
+        //TODO: update using textview ids.
+    }
+
+    /**
+     * Helper function used one time to initialize the database tables
+     */
+    private void setUpDatabase(){
+        //creates table
+        String setupStr = "CREATE TABLE effects ("
+                + "bracket INTEGER, overview TEXT, symptom TEXT, advice TEXT,"
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT"
+                + ");";
+        db.execSQL(setupStr);
+
+        //initialize values in table
+        //TODO: run insert commands here
     }
 }
