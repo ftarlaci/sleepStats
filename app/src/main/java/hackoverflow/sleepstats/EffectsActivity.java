d00aeeee247903bedafa73d2197e7ebe9b5c014a
Grace Hong <ghong97@stanford.edu>
2017-04-15 12:44:02 -0700

    added Effects activity

--- app/src/main/java/hackoverflow/sleepstats/EffectsActivity.java
+++ app/src/main/java/hackoverflow/sleepstats/EffectsActivity.java
@@ -1,13 +1,22 @@
 package hackoverflow.sleepstats;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class EffectsActivity extends AppCompatActivity {
 
+    SQLiteDatabase db;
+
+    /**
+     * FUNCTION: onCreate
+     * Overrides the onCreate method to initialize the database
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_effects);
+        openOrCreateDatabase("")
     }
 }
--- app/src/main/java/hackoverflow/sleepstats/MainActivity.java
+++ app/src/main/java/hackoverflow/sleepstats/MainActivity.java
@@ -1,10 +1,13 @@
 package hackoverflow.sleepstats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private double totalTime = 0; //initialized to zero, is reset after 'stop' is pushed.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +21,8 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void effectsButton(View view) {
+        Intent intent = new Intent(this, EffectsActivity.class);
+        intent.putExtra("totalTimeVal", totalTime);
+        startActivity(intent);
     }
 }
